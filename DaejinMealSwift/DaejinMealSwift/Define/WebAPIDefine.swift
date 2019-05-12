4fd0213554de9e1673b829a7be0d49a8810d1d1b
Hanwe Lee <lhanmw159@gmail.com>
2019-05-12 18:36:43 +0900

    파일누락 추가

---
+++ DaejinMealSwift/DaejinMealSwift/Define/WebAPIDefine.swift
@@ -0,0 +1,19 @@
+//
+//  WebAPIDefine.swift
+//  DaejinMealSwift
+//
+//  Created by hanwe on 12/05/2019.
+//  Copyright © 2019 hanwe. All rights reserved.
+//
+
+import UIKit
+
+class WebAPIDefine: NSObject {
+
+    //MARK: 대진대API
+    static public let DAEJIN_GET_MEAL_DATA_API_DEFINE:String = "http://www.daejin.ac.kr/front/commonsmenulist.do"
+    static public let DAEJIN_GET_MEAL_DATA_API_RQ_PARAM_YEAR_DEFINE:String = "rYear"
+    static public let DAEJIN_GET_MEAL_DATA_API_RQ_PARAM_MONTH_DEFINE:String = "rMonth"
+    static public let DAEJIN_GET_MEAL_DATA_API_RQ_PARAM_DAY_DEFINE:String = "rDay"
+    //MARK: 대진대API끝
+}
